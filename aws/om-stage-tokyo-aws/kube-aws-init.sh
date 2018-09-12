e305ccd550ad3c67579c2e21227c7d499d44c003
Jack Lee <initialjk@gmail.com>
2018-09-13 01:32:06 +0900

    no message

--- aws/om-stage-tokyo-aws/kube-aws-init.sh
+++ aws/om-stage-tokyo-aws/kube-aws-init.sh
@@ -3,7 +3,7 @@ kube-aws init \
  --region=ap-northeast-1\
  --availability-zone=ap-northeast-1a\
  --hosted-zone-id=Z2LWICAFHWTFD\
- --external-dns-name=stage.k8s.omgame.net\
+ --external-dns-name=k8s.stage.omgame.net\
  --key-name=kube-node\
  --kms-key-arn=arn:aws:kms:ap-northeast-1:902098765056:key/9a5aacca-6b6a-4a8d-b0c4-f7d0dc938cc3\
  --s3-uri=s3://om-kube-aws/
